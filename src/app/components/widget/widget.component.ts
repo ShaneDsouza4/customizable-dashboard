50013f57640fb88a1bf740223e800ab38346bf71
ShaneDsouza4 <shane.json96@gmail.com>
2024-09-24 19:13:01 -0400

    Cards height, row, move left, move right implemented

--- src/app/components/widget/widget.component.ts
+++ src/app/components/widget/widget.component.ts
@@ -1,14 +1,22 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { Widget } from '../../models/dashboard';
 import { CommonModule, NgComponentOutlet } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { WidgetOptionsComponent } from './widget-options/widget-options.component';
 
 @Component({
   selector: 'app-widget',
   standalone: true,
-  imports: [CommonModule, NgComponentOutlet],
+  imports: [CommonModule, NgComponentOutlet, MatButtonModule, MatIcon, WidgetOptionsComponent],
   templateUrl: './widget.component.html',
-  styleUrl: './widget.component.scss'
+  styleUrl: './widget.component.scss',
+  host: {
+    '[style.grid-area]': '"span " + (data().rows ?? 1) + "/ span " + (data().columns ?? 1)'
+  }
 })
 export class WidgetComponent {
   data = input.required<Widget>()
+
+  showOptions = signal(false)
 }
