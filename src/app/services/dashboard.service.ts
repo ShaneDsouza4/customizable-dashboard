86a5f14b62e17cc00ce87ead484be8c25f1dca81
ShaneDsouza4 <shane.json96@gmail.com>
2024-09-24 20:15:55 -0400

    Added chart

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,7 +1,10 @@
-import { computed, Injectable, signal } from '@angular/core';
+import { computed, effect, Injectable, signal } from '@angular/core';
 import { Widget } from '../models/dashboard';
 import { SubscribersComponent } from '../pages/dashboard/widgets/subscribers/subscribers.component';
 import { ViewsComponent } from '../pages/dashboard/widgets/views/views.component';
+import { WatchTimeComponent } from '../pages/dashboard/widgets/watch-time/watch-time.component';
+import { RevenueComponent } from '../pages/dashboard/widgets/revenue/revenue.component';
+import { AnalyticsComponent } from '../pages/dashboard/widgets/analytics/analytics.component';
 
 @Injectable()
 export class DashboardService {
@@ -10,35 +13,56 @@ export class DashboardService {
     {
       id: 1,
       label: 'Subscribers',
-      content: SubscribersComponent
+      content: SubscribersComponent,
+      rows: 1,
+      columns: 1,
+      backgroundColor: '#003f5c',
+      color: 'whitesmoke'
     },
     {
       id: 2,
       label: 'Views',
-      content: ViewsComponent
-    }
-  ])
-
-  addedWidgets = signal<Widget[]>([
+      content: ViewsComponent,
+      rows: 1,
+      columns: 1,
+      backgroundColor: '#003f5c',
+      color: 'whitesmoke'
+    },
     {
-      id: 1,
-      label: 'Subscribers',
-      content: SubscribersComponent,
+      id: 3,
+      label: 'Watch Time',
+      content: WatchTimeComponent,
+      rows: 1,
+      columns: 1,
+      backgroundColor: '#003f5c',
+      color: 'whitesmoke'
+    },
+    {
+      id: 4,
+      label: 'Revenue',
+      content: RevenueComponent,
+      rows: 1,
+      columns: 1,
+      backgroundColor: '#003f5c',
+      color: 'whitesmoke'
+    },
+    {
+      id: 5,
+      label: 'Analytics',
+      content: AnalyticsComponent,
       rows: 2,
       columns: 2
     },
-    {
-      id: 2,
-      label: 'Views',
-      content: ViewsComponent
-    }
-  ]);
+  ])
+
+  addedWidgets = signal<Widget[]>([]);
 
   widgetsToAdd = computed(() => {
     const addedIds = this.addedWidgets().map(w => w.id)
     return this.widgets().filter(w => !addedIds.includes(w.id));
 
   })
+
 
   addWidget(w: Widget) {
     this.addedWidgets.set([...this.addedWidgets(), { ...w }]) //Original stays untoched
@@ -77,5 +101,38 @@ export class DashboardService {
     this.addedWidgets.set(newWidgets)
   }
 
-  constructor() { }
+  removeWidget(id: number) {
+    this.addedWidgets.set(this.addedWidgets().filter(w => w.id !== id))
+  }
+
+  fetchWidgets() {
+    const widgetsAsString = localStorage.getItem('dashboardWidgets');
+    if (widgetsAsString) {
+      const widgets = JSON.parse(widgetsAsString) as Widget[];
+      widgets.forEach(widget => {
+        const content = this.widgets().find(w => w.id === widget.id)?.content;
+        if (content) {
+          widget.content = content;
+        }
+      })
+
+      this.addedWidgets.set(widgets)
+    }
+  }
+
+
+  constructor() {
+    this.fetchWidgets()
+  }
+
+
+  savedWidgets = effect(() => {
+    const widgetsWithoutContent: Partial<Widget>[] = this.addedWidgets().map(w => ({ ...w }));
+    widgetsWithoutContent.forEach(w => {
+      delete w.content;
+    })
+
+    localStorage.setItem('dashboardWidgets', JSON.stringify(widgetsWithoutContent))
+
+  })
 }
